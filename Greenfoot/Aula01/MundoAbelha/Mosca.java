e0a0fb30f3444c85bb09b6ab2ba9ea1433cb94fe
Kauan <k.richard.santos@gmail.com>
2025-06-03 21:43:52 -0300

    Aula01

---
+++ Greenfoot/Aula01/MundoAbelha/Mosca.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Mosca here.
+ * 
+ * @author (Kauan) 
+ * @version (03-06-2025)
+ */
+public class Mosca extends Actor
+{
+    /**
+     * Act - Método responsável pelas ações da mosca quando
+     * os botões Act ou Run são pressionados.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+        turn(1);
+    }
+}
