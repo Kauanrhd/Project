7c7e2414ce49c11e8965140d2fa61027705ec710
Kauan <k.richard.santos@gmail.com>
2025-06-23 21:54:01 -0300

    Aula13

---
+++ Eclipse/WorkSpace/25-java01/src/br/com/senaisp/bauru/aula06/Exemplo06.java
@@ -0,0 +1,12 @@
+package br.com.senaisp.bauru.aula06;
+
+public class Exemplo06 {
+
+	public static void main(String[] args) {
+		//Listando os argumentos passando para o programa 
+		for (String it: args) {
+			System.out.println(it);
+		}
+	}
+
+}
